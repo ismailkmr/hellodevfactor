d335a2891b7edeb15b87a1492b31d3540ac3441b
ithacher <ismail.tk18@gmail.com>
2022-06-08 16:30:47 +0530

    initial commit

---
+++ Node Basics/test.js
@@ -0,0 +1,36 @@
+function add() {
+  let x = 10;
+  let y = 20;
+  let z = x + y;
+  console.log(z);
+}
+function substract() {
+  let x = 10;
+  let y = 20;
+  let z = y - x;
+  console.log(z);
+}
+function decimel() {
+  let x = -5.2;
+  let y = 2.5;
+  let z = x - y;
+  console.log(z);
+}
+function multiplication() {
+  let x = 20;
+  let y = 5;
+  let z = x * y;
+  console.log(z);
+}
+function division(){
+    let x = 20;
+    let y = 5;
+    let z = x / y;
+    console.log(z);
+}
+add();
+substract();
+decimel();
+multiplication();
+division();
+
